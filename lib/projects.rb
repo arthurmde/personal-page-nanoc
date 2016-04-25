54c3bbbcf5acc591d5b13c60ea54389405aefd10
Arthur Del Esposte <arthurmde@gmail.com>
2016-04-25 10:23:08 -0300

    Add code base for posts

--- lib/projects.rb
+++ lib/projects.rb
@@ -1,3 +1,7 @@
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::HTMLEscape
 
 def projects
